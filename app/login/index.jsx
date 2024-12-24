7e5ed1e628e132ac4a307b79dd1c9d71e5a2486a
sameh-dh <samehdh44@gmail.com>
2024-12-24 22:42:57 +0100

    signin screen 70% of the desing

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,11 +1,20 @@
-import {  View, Text } from 'react-native'
+import { Image,View, Text } from 'react-native'
 import React from 'react'
 
 export default function LogInScreen() {
   return (
     <View>
-        
-      <Text>index</Text>
+      <Image source={require('./../../assets/images/login.png')}
+      style={{
+        width:'100%',
+        height:'500'
+      }}
+      />
+      <View style={{padding:20,display:'flex',alignItems:'center'}}>
+        <Text style={{fontFamily:'Outfit-Bold',fontSize:30}}>
+            Ready to make new freind ?
+        </Text>
+      </View>
     </View>
   )
 }
