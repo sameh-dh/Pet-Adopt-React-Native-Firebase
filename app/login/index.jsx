50d79daac64bbb32a44af01f6ce522641e679289
sameh-dh <samehdh44@gmail.com>
2024-12-29 12:45:17 +0100

    add pressable button

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,21 +1,57 @@
-import { Image,View, Text } from 'react-native'
-import React from 'react'
-
+import { Image, View, Text, Pressable } from "react-native";
+import React from "react";
+import Colors from "../../constants/colors.js";
 export default function LogInScreen() {
   return (
     <View>
-      <Image source={require('./../../assets/images/login.png')}
-      style={{
-        width:'100%',
-        height:'500'
-      }}
+      <Image
+        source={require("./../../assets/images/login.png")}
+        style={{
+          width: "100%",
+          height: "500",
+        }}
       />
-      <View style={{padding:20,display:'flex',alignItems:'center'}}>
-        <Text style={{fontFamily:'Outfit-Bold',fontSize:30,textAlign:'center'}}>
-            Ready to make new freind ?
+      <View style={{ padding: 20, display: "flex", alignItems: "center" }}>
+        <Text
+          style={{
+            fontFamily: "Outfit-Bold",
+            fontSize: 30,
+            textAlign: "center",
+          }}
+        >
+          Ready to make new freind ?
         </Text>
-        <Text style={{fontFamily:"Outfit", fontSize:18 ,textAlign:"center"}}>Let's adopt the pet wish you like and make there life happy again</Text>
+        <Text
+          style={{
+            fontFamily: "Outfit",
+            fontSize: 18,
+            textAlign: "center",
+            color: Colors.GRAY,
+          }}
+        >
+          Let's adopt the pet wish you like and make there life happy again
+        </Text>
+        <Pressable
+          style={{
+            backgroundColor: Colors.PRIMARY,
+            width: "100%",
+            height: "fit",
+            padding: 14,
+            borderRadius: 14,
+            marginTop: 10,
+            fontSize: 20,
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "Outfit-Bold",
+              textAlign: "center",
+            }}
+          >
+            Get Started
+          </Text>
+        </Pressable>
       </View>
     </View>
-  )
+  );
 }
